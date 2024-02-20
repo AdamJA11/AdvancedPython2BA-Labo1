82963cf76c8e4dd4a9ab38788b8b73ebb28e6498
AdamJA11 <22190@ecam.be>
2024-02-20 11:30:21 +0100

    Add unit test for utils module

--- test_utils.py
+++ test_utils.py
@@ -1,13 +1,13 @@
 import pytest
 import utils
 
-def test_fact():
-    # À compléter...
-    pass
 
+def test_fact():
+    
+    assert utils.fact(5) == 120
 def test_roots():
     # À compléter...
-    pass
+    assert utils.roots(1,0,1)
 
 def test_integrate():
     # À compléter...
