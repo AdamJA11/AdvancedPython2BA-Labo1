91e1d97c0bb4cdb31e689fa9002f5606a13b9155
AdamJA11 <22190@ecam.be>
2024-02-20 11:00:19 +0100

    Add unit test for utils module

--- utils.py
+++ utils.py
@@ -1,11 +1,11 @@
 def fact(n):
-	"""Computes the factorial of a natural number.
+	a = 1
+	for i in range(1, n+1):
+		a = i *a
+	return 	a
+		
 	
-	Pre: -
-	Post: Returns the factorial of 'n'.
-	Throws: ValueError if n < 0
-	"""
-	pass
+
 
 def roots(a, b, c):
 	"""Computes the roots of the ax^2 + bx + x = 0 polynomial.
